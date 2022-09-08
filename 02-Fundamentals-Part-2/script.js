8551eba212feb44d0ff3e457541b48c30ea9c899
chaehyun_oh <ohchaehh@gmail.com>
2022-09-08 21:25:00 +0900

    220908 update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -32,23 +32,96 @@ console.log(fruitProcessor(5, 0))
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
-*/
 
 // function calcAge1(birthYear) {
-//     const age = 2022 - birthYear;
-//     return age;
-// }
+    //     const age = 2022 - birthYear;
+    //     return age;
+    // }
 
-function calcAge1(birthYear) {
-    return 2022 - birthYear;
+    function calcAge1(birthYear) {
+        return 2022 - birthYear;
+    }
+
+    const age1 = calcAge1(1991);
+    // console.log(age1)
+
+    const calcAge2 = function (birthYear) {
+        return 2022 - birthYear;
+    }
+
+    const age2 = calcAge2(1991);
+    console.log(age1, age2);
+
+    const calcAge3 = brithYear => 2037 - brithYear;
+    const age3 = calcAge3(1991);
+    console.log(age3);
+
+    const yearsUntilRetirement = (birthYear, firstName) => {
+        const age = 2022 - birthYear;
+        const retirement = 65 - age;
+        // return retirement;
+        return `${firstName} retires in ${retirement} years`
+    }
+    // yearsUntilRetirement(1991);
+
+    console.log(yearsUntilRetirement(1991, 'Jonas'))
+
+    function cutFruitPieces(fruit) {
+        return fruit * 4;
+    }
+
+    function fruitProcessor(apples, oranges) {
+        const applePieces = cutFruitPieces(apples);
+        const orangePieces = cutFruitPieces(oranges);
+
+        const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} piece of orange.`;
+        return juice;
+    }
+
+    console.log(fruitProcessor(2, 3))
+
+    const calcAge = function (birthYear) {
+        return 2022 - birthYear;
+    }
+
+    const yearsUntilRetirement = function (birthYear, firstName) {
+        const age = calcAge(birthYear);
+        const retirement = 65 - age;
+
+        if (retirement > 0) {
+            console.log(`${firstName} retires in ${retirement} years`)
+            return retirement;
+        } else {
+            console.log(`${firstName} has already retired.`)
+            return -1;
+        }
+
+    }
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1981, 'Mike'));
+console.log(yearsUntilRetirement(1955, 'Jack'));
+*/
+
+//  # Coding challenge #1
+function calcAverage(team) {
+    const teamAvg = team / 3;
+    return teamAvg;
 }
 
-const age1 = calcAge1(1991);
-// console.log(age1)
+const calcAverage2 = (a, b, c) => (a + b + c) / 3;
 
-const calcAge2 = function (birthYear) {
-    return 2022 - birthYear;
+function checkWinner(avg1, avg2) {
+    if (avg1 >= avg2 * 2) {
+        console.log(`Dolphins win (${avg1} vs. ${avg2})`);
+    } else if (avg2 >= avg1 * 2) {
+        console.log(`Koalas win (${avg2} vs. ${avg1})`);
+    } else {
+        console.log('No team wins...')
+    }
 }
 
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+const Dolphins = 85 + 54 + 41;
+const Koalas = 23 + 34 + 27;
+const DolAvg = calcAverage(Dolphins);
+const KoAvg = calcAverage(Koalas);
+console.log(checkWinner(DolAvg, KoAvg))
--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 # TIL
 
-| Day                              | Title                                            |
-| -------------------------------- | ------------------------------------------------ |
-| [Day 1](./markdown/cjs220830.md) | S1. Start                                        |
-| [Day 2](./markdown/cjs220831.md) | S2. JavaScript Fundamentals - Part 1 ( 9 ~ 11 )  |
-| [Day 3](./markdown/cjs220901.md) | S2. JavaScript Fundamentals - Part 1 ( 12 ~ 14 ) |
-| [Day 4](./markdown/cjs220902.md) | S2. JavaScript Fundamentals - Part 1 ( 15 ~ 17 ) |
-| [Day 5](./markdown/cjs220903.md) | S2. JavaScript Fundamentals - Part 1 ( 18 ~ 20 ) |
-| [Day 6](./markdown/cjs220904.md) | S2. JavaScript Fundamentals - Part 1 ( 21 ~ 23 ) |
-| [Day 7](./markdown/cjs220905.md) | S2. JavaScript Fundamentals - Part 1 ( 24 ~ 26 ) |
-| [Day 8](./markdown/cjs220906.md) | S2. JavaScript Fundamentals - Part 1 ( 27 ~ 30 ) |
-| [Day 9](./markdown/cjs220907.md) | S3. JavaScript Fundamentals - Part 2 ( 31 ~ 34 ) |
-|                                  |                                                  |
+| Day                               | Title                                            |
+| --------------------------------- | ------------------------------------------------ |
+| [Day 1](./markdown/cjs220830.md)  | S1. Start                                        |
+| [Day 2](./markdown/cjs220831.md)  | S2. JavaScript Fundamentals - Part 1 ( 9 ~ 11 )  |
+| [Day 3](./markdown/cjs220901.md)  | S2. JavaScript Fundamentals - Part 1 ( 12 ~ 14 ) |
+| [Day 4](./markdown/cjs220902.md)  | S2. JavaScript Fundamentals - Part 1 ( 15 ~ 17 ) |
+| [Day 5](./markdown/cjs220903.md)  | S2. JavaScript Fundamentals - Part 1 ( 18 ~ 20 ) |
+| [Day 6](./markdown/cjs220904.md)  | S2. JavaScript Fundamentals - Part 1 ( 21 ~ 23 ) |
+| [Day 7](./markdown/cjs220905.md)  | S2. JavaScript Fundamentals - Part 1 ( 24 ~ 26 ) |
+| [Day 8](./markdown/cjs220906.md)  | S2. JavaScript Fundamentals - Part 1 ( 27 ~ 30 ) |
+| [Day 9](./markdown/cjs220907.md)  | S3. JavaScript Fundamentals - Part 2 ( 31 ~ 34 ) |
+| [Day 10](./markdown/cjs220908.md) | S3. JavaScript Fundamentals - Part 2 ( 35 ~ 38 ) |
+|                                   |                                                  |
