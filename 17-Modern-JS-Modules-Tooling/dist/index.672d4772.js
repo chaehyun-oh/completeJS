e15bb2eace3703118698676ead06e078820fd61f
chaehyun_oh <ohchaehh@gmail.com>
2022-12-05 23:59:43 +0900

    221205 update

--- 17-Modern-JS-Modules-Tooling/dist/index.672d4772.js
+++ 17-Modern-JS-Modules-Tooling/dist/index.672d4772.js
@@ -1,34 +1,24 @@
 // Importing module
 // import { addToCart, totalPrice as price, totalQuantity } from "./shoppingCart.js";
-
 // addToCart('bread', 5);
 // console.log(price, totalQuantity);
-
-console.log('Importing module');
-
+console.log("Importing module");
 // import * as ShoppingCart from './shoppingCart.js';
-
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
-
 // import add, { totalPrice as price, totalQuantity } from './shoppingCart.js'; --- this is not desirable
-
 // import add, { cart } from './shoppingCart.js';
 // add('pizza', 2);
 // add('apples', 4);
 // add('lemon', 8);
-
 // // import is not a copy
 // console.log(cart);
-
 // ///////////////////
-
 // console.log('start fetching');
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 // const data = await res.json();
 // console.log(data);
 // console.log('something');
-
 /*
 const getLastPost = async function () {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -100,9 +90,6 @@ const stateDeepClone = cloneDeep(state);
 state.user.loggedIn = false;
 console.log(stateClone);
 console.log(stateDeepClone);
-*/
+*/ if (module.hot) module.hot.accept();
 
-
-if (module.hot) {
-    module.hot.accept()
-}
+//# sourceMappingURL=index.672d4772.js.map
