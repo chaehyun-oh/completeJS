73c4f7c531a8ff4b02a7679486558cdd61d4b419
chaehyun_oh <ohchaehh@gmail.com>
2022-12-24 18:55:18 +0900

    221224 update

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -27,6 +27,9 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
+    //  0. Update results view to mark selected search result
+    resultsView.update(model.getSearchResultPage());
+
     //  1. loading recipe
     await model.loadRecipe(id);
     // const { recipe } = model.state;
@@ -77,7 +80,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   //  Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 
 }
 
--- 18-forkify/src/js/views/resultsView.js
+++ 18-forkify/src/js/views/resultsView.js
@@ -2,19 +2,21 @@ import icons from 'url:../../img/icons.svg';
 import View from "./View.js";
 
 class ResultsView extends View {
-    _parentElement = document.querySelector('.results');
-    _errorMessage = 'No recipes found for your query! Please try again';
-    _message = '';
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again';
+  _message = '';
 
-    _generateMarkup() {
-        console.log(this._data);
-        return this._data.map(this._generateMarkupPreview).join('')
-    }
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(this._generateMarkupPreview).join('')
+  }
 
-    _generateMarkupPreview(result) {
-        return `
+  _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
+
+    return `
         <li class="preview">
-            <a class="preview__link" href="#${result.id}">
+            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="${result.title}" />
               </figure>
@@ -25,7 +27,7 @@ class ResultsView extends View {
             </a>
           </li>
         `
-    }
+  }
 
 }
 
